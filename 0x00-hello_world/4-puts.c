bf79f27d52176902133a9f5a414e86d5ea92d250
walidmeniari <oualid.meniari@gmail.com>
2023-09-07 12:28:56 -0700

    lol

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - main entry point
+ *
+ * Return: Always 0 (Succes)s
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
