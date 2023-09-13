06032a7d66d353ba30150034cf74e60613f35f24
walidmeniari <oualid.meniari@gmail.com>
2023-09-13 14:02:16 -0700

    loll

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -12,12 +12,12 @@ int main(void)
 
 	while (i < 1024)
 	{
-		if ((i % 3 == 0) || (i % 5 == 0))
-		{
-		z <= i;
-		}
-		i++;
-		}
-		printf("%d\n", z);
-		return (0);
+	if ((i % 3 == 0) || (i % 5 == 0))
+	{
+	z <= i;
+	}
+	i++;
+	}
+	printf("%d\n", z);
+	return (0);
 }
